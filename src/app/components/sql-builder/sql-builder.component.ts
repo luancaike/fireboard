40d2e7a9174096f9fba5cc173272c7cc8010542d
luancaike <luan.caike@acad.ifma.edu.br>
2021-08-02 15:20:02 -0300

    feat: finalized sql builder implementation

--- src/app/components/sql-builder/sql-builder.component.ts
+++ src/app/components/sql-builder/sql-builder.component.ts
@@ -1,8 +1,18 @@
-import { Component, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, Output, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
 import { PopoverComponent } from '../popover/popover.component';
 import { DataSourceMockList } from '../../models/mocks';
-import { DataSource } from '../../models/data-source.dtos';
-import { JoinType } from './sql-builder.model';
+import { DataSource, DataSourceKey, DataSourceKeyTypes } from '../../models/data-source.dtos';
+import {
+    FilterOperator,
+    FilterOperatorsDate,
+    FilterOperatorsNumber,
+    FilterOperatorsString,
+    FilterValuesSqlBuild,
+    getDefaultModel,
+    getDefaultModelJoin,
+    JoinType,
+    ModelSqlBuild
+} from './sql-builder.model';
 
 @Component({
     selector: 'fb-sql-builder',
@@ -12,24 +22,27 @@ import { JoinType } from './sql-builder.model';
 })
 export class SqlBuilderComponent {
     @ViewChildren(PopoverComponent) popovers: QueryList<PopoverComponent>;
-    public tables: DataSource[] = DataSourceMockList;
-    public model = {
-        table: null,
-        top: 0,
-        select: [],
-        group: [],
-        order: [],
-        forks: [
-            {
-                table: null,
-                joinType: JoinType.Left,
-                columnPrimary: null,
-                columnSecondary: null
-            }
-        ]
-    };
+    @Output() showPanelChange = new EventEmitter();
+    @Output() save = new EventEmitter<ModelSqlBuild>();
 
-    get columnsOfModel() {
+    @Input() set showPanel(value: boolean) {
+        this.showPanelChange.emit(value);
+        this._showPanel = value;
+    }
+
+    get showPanel(): boolean {
+        return this._showPanel;
+    }
+
+    private _showPanel = false;
+    public tables: DataSource[] = DataSourceMockList;
+    public selectedColumnFilter: DataSourceKey;
+    public selectedOperator;
+    public operatorsFilter: FilterOperator[] = [];
+    public operatorsValues: FilterValuesSqlBuild[] = [];
+    public model: ModelSqlBuild = getDefaultModel();
+
+    get tablesOfModel() {
         const baseColumns = this.model?.table ? [this.model?.table] : [];
         const forkColumns =
             this.model?.forks?.reduce((acc, item) => {
@@ -41,12 +54,31 @@ export class SqlBuilderComponent {
         return [...baseColumns, ...forkColumns];
     }
 
+    get filterIsValid(): boolean {
+        return this.selectedColumnFilter && this.selectedOperator && this.operatorsValues.every((el) => !!el.value);
+    }
+
+    get columnsOfModel() {
+        return this.tablesOfModel.reduce((acc, item) => {
+            return [...acc, ...item.keys.map((el) => ({ ...el, name: `${item.name} -> ${el.name}` }))];
+        }, []);
+    }
+
+    public trackByValorFilter = (index: number, item: any) => {
+        return item;
+    };
+
     public selectTable = (table: DataSource) => {
+        this.model = getDefaultModel();
         this.model.table = table;
         this.closeAllPopovers();
     };
-    public addSelect = (table: DataSource) => {
+    public addSelect = (table: DataSourceKey) => {
         this.model.select.push(table);
+        this.closeAllPopovers();
+    };
+    public addFork = () => {
+        this.model.forks.push(getDefaultModelJoin());
         this.closeAllPopovers();
     };
     public addGroup = (table: any) => {
@@ -78,9 +110,16 @@ export class SqlBuilderComponent {
         this.popovers.forEach((popover) => popover.hide());
     }
 
+    resetFilterSelector(): void {
+        this.selectedColumnFilter = undefined;
+        this.selectedOperator = undefined;
+        this.operatorsFilter = [];
+        this.operatorsValues = [];
+    }
+
     openBox(element: HTMLElement) {
         if (!!~element.className.indexOf('collapsed')) {
-            element.classList.toggle('collapsed');
+            element.classList.remove('collapsed');
         }
     }
 
@@ -90,5 +129,77 @@ export class SqlBuilderComponent {
         } else {
             ++this.model.forks[index].joinType;
         }
+    }
+
+    onSelectedFilter(filter: DataSourceKey) {
+        this.selectedColumnFilter = filter;
+        switch (this.selectedColumnFilter.type) {
+            case DataSourceKeyTypes.Boolean:
+                break;
+            case DataSourceKeyTypes.String:
+                this.operatorsFilter = FilterOperatorsString;
+                break;
+            case DataSourceKeyTypes.Date:
+                this.operatorsFilter = FilterOperatorsDate;
+                break;
+            case DataSourceKeyTypes.Number:
+                this.operatorsFilter = FilterOperatorsNumber;
+                break;
+            case DataSourceKeyTypes.Custom:
+                break;
+        }
+    }
+
+    addFilter() {
+        const model = {
+            values: this.operatorsValues,
+            operator: this.selectedOperator,
+            column: this.selectedColumnFilter
+        };
+        console.log(model);
+        this.model.filters.push(model);
+        this.resetFilterSelector();
+    }
+
+    mapValuesToString(values: any[]): string {
+        const valueIsDate = (value: any) => typeof value === 'string' && new Date(value).toString() !== 'Invalid Date';
+        return values
+            .map(({ value }) => (valueIsDate(value) ? new Date(value).toLocaleDateString('pt-Br') : value))
+            .toString();
+    }
+
+    onSelectedOperator(operator: FilterOperator) {
+        this.operatorsValues = new Array(operator.amountValues).fill(null).map(() => ({
+            value: null,
+            type: this.selectedColumnFilter.type
+        }));
+    }
+
+    closePanel() {
+        this.showPanel = false;
+        this.model = getDefaultModel();
+    }
+
+    clearModelField(field: string, element: HTMLElement) {
+        element.classList.add('collapsed');
+        if (field === 'forks') {
+            this.model.forks = [
+                {
+                    table: null,
+                    joinType: JoinType.Left,
+                    columnPrimary: null,
+                    columnSecondary: null
+                }
+            ];
+        } else if (Array.isArray(this.model[field])) {
+            this.model[field] = [];
+        } else {
+            this.model[field] = null;
+        }
+    }
+
+    saveThisModel() {
+        this.save.emit(this.model);
+        this.closePanel();
     }
 }
--- src/app/components/sql-builder/sql-builder.model.ts
+++ src/app/components/sql-builder/sql-builder.model.ts
@@ -1,5 +1,221 @@
+import { DataSource, DataSourceKey, DataSourceKeyTypes } from '../../models/data-source.dtos';
+
 export enum JoinType {
     Left = 1,
     Right = 2,
     Inner = 3
 }
+
+export enum FilterOperatorTypes {
+    GreaterThan = 1,
+    GreaterThanOrEqual = 2,
+    LessThan = 3,
+    LessThanOrEqual = 4,
+    Between = 5,
+    NotBetween = 6,
+    Equal = 7,
+    NotEqual = 8,
+    In = 9,
+    NotIn = 10,
+    IsEmpty = 11,
+    IsNotEmpty = 12,
+    IsNull = 13,
+    IsNotNull = 14,
+    StartsWith = 15,
+    EndsWith = 16,
+    Contains = 17,
+    NotContains = 18,
+    Before = 19,
+    After = 20
+}
+
+export interface FilterOperator {
+    id: FilterOperatorTypes;
+    text: string;
+    amountValues: number;
+}
+
+export const FilterOperatorsNumber: FilterOperator[] = [
+    {
+        id: FilterOperatorTypes.GreaterThan,
+        text: 'Maior do que',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.GreaterThanOrEqual,
+        text: 'Maior que ou igual a',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.LessThan,
+        text: 'Menor que',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.LessThanOrEqual,
+        text: 'Menor que ou igual a',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.Between,
+        text: 'Entre',
+        amountValues: 2
+    },
+    {
+        id: FilterOperatorTypes.NotBetween,
+        text: 'Não está entre',
+        amountValues: 2
+    },
+    {
+        id: FilterOperatorTypes.Equal,
+        text: 'É',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.NotEqual,
+        text: 'Não é',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.IsEmpty,
+        text: 'Vazio',
+        amountValues: 0
+    },
+    {
+        id: FilterOperatorTypes.IsNotEmpty,
+        text: 'Não Vazio',
+        amountValues: 0
+    }
+];
+export const FilterOperatorsString: FilterOperator[] = [
+    {
+        id: FilterOperatorTypes.Equal,
+        text: 'É',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.NotEqual,
+        text: 'Não é',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.Contains,
+        text: 'Contém',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.NotContains,
+        text: 'Não Contém',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.StartsWith,
+        text: 'Começa com',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.EndsWith,
+        text: 'Termina em',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.IsEmpty,
+        text: 'Vazio',
+        amountValues: 0
+    },
+    {
+        id: FilterOperatorTypes.IsNotEmpty,
+        text: 'Não Vazio',
+        amountValues: 0
+    }
+];
+export const FilterOperatorsDate: FilterOperator[] = [
+    {
+        id: FilterOperatorTypes.Equal,
+        text: 'É',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.NotEqual,
+        text: 'Não é',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.Between,
+        text: 'Entre',
+        amountValues: 2
+    },
+    {
+        id: FilterOperatorTypes.NotBetween,
+        text: 'Não está entre',
+        amountValues: 2
+    },
+    {
+        id: FilterOperatorTypes.Before,
+        text: 'Antes',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.After,
+        text: 'Depois',
+        amountValues: 1
+    },
+    {
+        id: FilterOperatorTypes.IsEmpty,
+        text: 'Vazio',
+        amountValues: 0
+    },
+    {
+        id: FilterOperatorTypes.IsNotEmpty,
+        text: 'Não Vazio',
+        amountValues: 0
+    }
+];
+
+export interface JoinSqlBuild {
+    columnPrimary: DataSourceKey;
+    columnSecondary: DataSourceKey;
+    joinType: JoinType;
+    table: DataSource;
+}
+
+export interface FilterValuesSqlBuild {
+    value: any;
+    type: DataSourceKeyTypes;
+}
+
+export interface FilterSqlBuild {
+    values: FilterValuesSqlBuild[];
+    operator: FilterOperator;
+    column: DataSourceKey;
+}
+
+export interface OrderSqlBuild extends DataSourceKey {
+    direction: 'up' | 'down';
+}
+
+export interface ModelSqlBuild {
+    table: DataSource;
+    top: number;
+    select: DataSourceKey[];
+    group: DataSourceKey[];
+    order: OrderSqlBuild[];
+    filters: FilterSqlBuild[];
+    forks: JoinSqlBuild[];
+}
+
+export const getDefaultModel = (): ModelSqlBuild => ({
+    table: null,
+    top: 0,
+    select: [],
+    group: [],
+    order: [],
+    filters: [],
+    forks: []
+});
+export const getDefaultModelJoin = () => ({
+    table: null,
+    joinType: JoinType.Left,
+    columnPrimary: null,
+    columnSecondary: null
+});
